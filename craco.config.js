d16e6ffde809f06884ba4391a561e5d72a7c5565
agent1337 <agent1337expert@gmail.com>
2022-07-22 11:24:59 +0300

    craco config

--- craco.config.js
+++ craco.config.js
@@ -5,8 +5,12 @@ module.exports = {
     alias: {
       "@components": path.resolve(__dirname, "src/components"),
       "@custom": path.resolve(__dirname, "src/custom"),
+      "@pages": path.resolve(__dirname, "src/pages"),
       "@redux": path.resolve(__dirname, "src/redux"),
       "@routing": path.resolve(__dirname, "src/routing"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@helpers": path.resolve(__dirname, "src/helpers"),
+      "@images": path.resolve(__dirname, "public"),
     },
   },
 };
